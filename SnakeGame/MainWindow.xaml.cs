d3eeab481bc91cd99349f73eccb4f2c8cd4e7c8c
Hugo Raczka <M8LN@eurofins.com>
2023-03-31 09:45:03 +0200

    Created the GameBoard Designer and the empty (so far) SnakeElement Class

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -12,17 +12,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SnakeGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        int _elementSize = 20;
+        DispatcherTimer _gameLoopTimer;
+        
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
+            DrawGameWorld();
         }
+
+        private void DrawGameWorld() {
+            var numberOfColums = this.Width / _elementSize;
+            var numberOfRows = this.Height / _elementSize;
+
+            for (int i = 0; i < numberOfRows; i++) {
+                Line line = new Line();
+                line.Stroke = Brushes.Black;
+                line.X1 = 0;
+                line.Y1 = i * _elementSize;
+                line.X2 = Width;
+                line.Y2 = i * _elementSize;
+                GameWorld.Children.Add(line);
+            }
+            for (int i = 0; i < numberOfColums; i++) {
+                Line line = new Line();
+                line.Stroke = Brushes.Black;
+                line.X1 = i * _elementSize;
+                line.Y1 = 0;
+                line.X2 = i * _elementSize  ;
+                line.Y2 = Height;
+                GameWorld.Children.Add(line);
+            }
+
+        }
+
+        public void InitializeTimer() {
+            _gameLoopTimer = new DispatcherTimer();
+            _gameLoopTimer.Interval = TimeSpan.FromSeconds(0.5);
+            //_gameLoopTimer.Tick += MainGameLoop();
+            _gameLoopTimer.Start();
+        }
+
+      
     }
 }
