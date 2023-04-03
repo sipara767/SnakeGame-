dd135bd1e5bfdbeac5ba333a6f8a489ca3ba26be
Hugo Raczka <M8LN@eurofins.com>
2023-04-03 15:42:51 +0200

    First tries of MVVM

---
+++ Snake.NET6/Model/SnakeElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+
+namespace Snake.NET6.Model {
+    internal class SnakeElement : ObservableObject {
+        
+        private int x;
+
+        public int X {
+            get { return x; }
+            set { SetProperty(ref x, value); }
+        }
+
+
+        private int y;
+
+        public int Y {
+            get { return y; }
+            set { SetProperty(ref y, value); }
+        }
+
+    }
+}
---
+++ Snake.NET6/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Windows.Input;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace Snake.NET6.ViewModel {
+    internal class MainViewModel : ObservableObject {
+        private Model.SnakeElement snake;
+
+        public Model.SnakeElement Snake {
+            get { return snake; }
+            set { SetProperty(ref snake, value); }
+        }
+
+        public ICommand MoveUp { get; }
+        public ICommand MoveRight { get; }
+        public ICommand MoveDown { get; }
+        public ICommand MoveLeft { get; }
+
+
+        public MainViewModel() {
+            snake = new Model.SnakeElement();
+            MoveUp = new RelayCommand(SnakeMoveUp);
+            MoveRight = new RelayCommand(SnakeMoveRight);
+            MoveDown = new RelayCommand(SnakeMoveDown);
+            MoveLeft = new RelayCommand(SnakeMoveLeft);
+        }
+        private void SnakeMoveUp() {
+            Snake.Y += 1;
+        }
+
+        private void SnakeMoveRight() {
+            Snake.X += 1;
+        }
+
+        private void SnakeMoveDown() {
+            Snake.Y -= 1;
+        }
+
+        private void SnakeMoveLeft() {
+            Snake.Y -= 1;
+        }
+
+    }
+}
